69ca326b88a443a9de45b62ac26af0604cc56691
Dylan Wicker <dylanwicker27@gmail.com>
2024-05-30 16:48:16 -0400

    added layout and navbar

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,9 @@
 import { ThemeProvider, createTheme } from "@mui/material/styles"
 import { useMemo } from "react"
 import { themeSettings } from "./theme"
-import { CssBaseline } from "@mui/material"
+import { Box, CssBaseline } from "@mui/material"
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Navbar from "@scenes/navbar";
 
 function App() {
   const theme = useMemo(()=> createTheme(themeSettings), [])
@@ -10,10 +12,18 @@ function App() {
   return (
     <>
       <div className = "app">
+        <BrowserRouter>
         <ThemeProvider theme={theme}>
           <CssBaseline />
-          hi
+          <Box width="100%" height="100%" padding ="1rem 2rem 4rem 2rem" >
+            <Navbar />
+            <Routes>
+              <Route path ="/" element={<div>dashboard page</div>} />
+              <Route path ="/predictions" element={<div>predictions page</div>} />
+            </Routes>
+          </Box>
         </ThemeProvider>
+        </BrowserRouter>
       </div>
     </>
   )
