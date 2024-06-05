644b02e57e96b31692fec9c1b2f111e52254e67d
Dylan Wicker <dylanwicker27@gmail.com>
2024-06-05 14:01:12 -0400

    added transactions route

--- server/index.js
+++ server/index.js
@@ -7,10 +7,10 @@ import helmet from "helmet";
 import morgan from "morgan";
 import kpiRoutes from "./routes/kpi.js";
 import productRoutes from "./routes/product.js";
-// import transactionRoutes from "./routes/transaction.js";
+import transactionRoutes from "./routes/transaction.js";
 import KPI from "./models/KPI.js";
 import Product from "./models/Product.js";
-// import Transaction from "./models/Transaction.js";
+import Transaction from "./models/Transaction.js";
 import { kpis, products, transactions } from "./data/data.js";
 
 
@@ -26,7 +26,7 @@ app.use(cors());
 
 app.use("/kpi", kpiRoutes);
 app.use("/product", productRoutes);
-// app.use("/transaction", transactionRoutes);
+app.use("/transaction", transactionRoutes);
 
 const PORT = process.env.PORT || 9000;
 mongoose
